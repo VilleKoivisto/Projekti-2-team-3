3c2d2492765236eee4de87f7131041bf0c250680
Ville <koivisto.vt@gmail.com>
2021-11-24 22:47:10 +0200

    Blogin toimiva versio ja Kuben yamlit blogi-appin pystyttämistä varten

--- blogisofta/get_login.py
+++ blogisofta/get_login.py
@@ -10,19 +10,20 @@ def login_data():
     """
 
     # Luo yhteys
-    client = secretmanager.SecretManagerServiceClient()
+    # client = secretmanager.SecretManagerServiceClient()
 
-    # Määrittele salaisuuden polku:
-    # projects/<PROJEKTI>/secrets/<SALAISUUS>/versions/latest
-    usernm = "KÄYTTÄJÄNIMEN POLKU TÄHÄN"
-    passwd = "PASSUPOLKU TÄHÄN"
+    # # Määrittele salaisuuden polku:
+    # # projects/<PROJEKTI>/secrets/<SALAISUUS>/versions/latest
+    # usernm = "KÄYTTÄJÄNIMEN POLKU TÄHÄN"
+    # passwd = "PASSUPOLKU TÄHÄN"
 
-    # Avaa access salaisuuteen
-    response_un = client.access_secret_version(request={"name": usernm})
-    response_pw = client.access_secret_version(request={"name": passwd})
+    # # Avaa access salaisuuteen
+    # response_un = client.access_secret_version(request={"name": usernm})
+    # response_pw = client.access_secret_version(request={"name": passwd})
 
-    # Tulosta salaisuuden sisältö
-    payload_un = response_un.payload.data.decode("UTF-8")
-    payload_pw = response_pw.payload.data.decode("UTF-8")
+    # # Tulosta salaisuuden sisältö
+    # payload_un = response_un.payload.data.decode("UTF-8")
+    # payload_pw = response_pw.payload.data.decode("UTF-8")
     
-    return payload_un, payload_pw
+    # return payload_un, payload_pw
+    return "<KÄYTTÄJÄNIMI TÄHÄN>", "<SALASANA TÄHÄN>"
--- blogisofta/query_config.py
+++ blogisofta/query_config.py
@@ -9,6 +9,6 @@ def config():
     user_name, passwd = login_data()
 
     # host täytyy muuttaa vastaamaan postgresin ip:tä kubessa:
-    db = {'host': '127.0.0.1', 'database': 'blogi', 'port': '5432', 'user': user_name, 'password': passwd}
+    db = {'host': '<SQL-IP TÄHÄN>', 'database': 'blogi', 'port': '5432', 'user': user_name, 'password': passwd}
     
     return db
